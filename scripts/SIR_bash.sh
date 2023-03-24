efb572374286d26a359285d3be78a608d249d62d
Zonghao Chen <47094594+hudsonchen@users.noreply.github.com>
2023-03-24 15:18:42 +0000

    minor

---
+++ scripts/SIR_bash.sh
@@ -0,0 +1,23 @@
+#$ -l tmem=30G
+#$ -l h_vmem=30G
+#$ -l h_rt=12:0:0
+#$ -R y
+
+#$ -S /bin/bash
+#$ -j y
+#$ -N cbq_SIR
+
+#source /share/apps/source_files/python/python-3.8.5.source
+JOB_PARAMS=$(sed "${SGE_TASK_ID}q;d" "$1")
+echo "Job params: $JOB_PARAMS"
+
+conda activate cbq
+
+date
+
+## Check if the environment is correct.
+which pip
+which python
+
+pwd
+python /home/zongchen/CBQ/SIR.py $JOB_PARAMS
