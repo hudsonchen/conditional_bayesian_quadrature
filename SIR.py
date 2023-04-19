35be70166727ffa46c3acf6b4dedc2e43f32c788
Zonghao Chen <47094594+hudsonchen@users.noreply.github.com>
2023-04-19 17:37:01 +0100

    SIR_final

--- SIR.py
+++ SIR.py
@@ -58,21 +58,21 @@ def Bayesian_Monte_Carlo(rng_key, y, gy, d_log_py, kernel_y):
     A_init = A = 1.0 / jnp.sqrt(n)
     opt_state = optimizer.init((l_init, c_init, A_init))
 
-    @jax.jit
-    def nllk_func(l, c, A):
-        l, c, A = l, c, A
-        n = y.shape[0]
-        K = A * kernel_y(y, y, l, d_log_py, d_log_py) + c + A * jnp.eye(n)
-        K_inv = jnp.linalg.inv(K + eps * jnp.eye(n))
-        nll = -(-0.5 * gy.T @ K_inv @ gy - 0.5 * jnp.log(jnp.linalg.det(K) + eps)) / n
-        return nll
-
-    @jax.jit
-    def step(l, c, A, opt_state, rng_key):
-        nllk_value, grads = jax.value_and_grad(nllk_func, argnums=(0, 1, 2))(l, c, A)
-        updates, opt_state = optimizer.update(grads, opt_state, (l, c, A))
-        l, c, A = optax.apply_updates((l, c, A), updates)
-        return l, c, A, opt_state, nllk_value
+    # @jax.jit
+    # def nllk_func(l, c, A):
+    #     l, c, A = l, c, A
+    #     n = y.shape[0]
+    #     K = A * kernel_y(y, y, l, d_log_py, d_log_py) + c + A * jnp.eye(n)
+    #     K_inv = jnp.linalg.inv(K)
+    #     nll = -(-0.5 * gy.T @ K_inv @ gy - 0.5 * jnp.log(jnp.linalg.det(K) + eps)) / n
+    #     return nll
+    #
+    # @jax.jit
+    # def step(l, c, A, opt_state, rng_key):
+    #     nllk_value, grads = jax.value_and_grad(nllk_func, argnums=(0, 1, 2))(l, c, A)
+    #     updates, opt_state = optimizer.update(grads, opt_state, (l, c, A))
+    #     l, c, A = optax.apply_updates((l, c, A), updates)
+    #     return l, c, A, opt_state, nllk_value
 
     # ========== Debug code ==========
     l_debug_list = []
@@ -81,27 +81,28 @@ def Bayesian_Monte_Carlo(rng_key, y, gy, d_log_py, kernel_y):
     nll_debug_list = []
     # ========== Debug code ==========
 
-    for _ in range(100):
-        rng_key, _ = jax.random.split(rng_key)
-        l, c, A, opt_state, nllk_value = step(l, c, A, opt_state, rng_key)
-
+    # for _ in range(0):
+    #     rng_key, _ = jax.random.split(rng_key)
+    #     l, c, A, opt_state, nllk_value = step(l, c, A, opt_state, rng_key)
+    #     if jnp.isnan(nllk_value):
+    #         break
     # ========== Debug code ==========
-        l_debug_list.append(l)
-        c_debug_list.append(c)
-        A_debug_list.append(A)
-        nll_debug_list.append(nllk_value)
-    fig = plt.figure(figsize=(15, 6))
-    ax_1, ax_2, ax_3, ax_4 = fig.subplots(1, 4)
-    ax_1.plot(l_debug_list)
-    ax_2.plot(c_debug_list)
-    ax_3.plot(A_debug_list)
-    ax_4.plot(nll_debug_list)
-    plt.show()
+    #     l_debug_list.append(l)
+    #     c_debug_list.append(c)
+    #     A_debug_list.append(A)
+    #     nll_debug_list.append(nllk_value)
+    # fig = plt.figure(figsize=(15, 6))
+    # ax_1, ax_2, ax_3, ax_4 = fig.subplots(1, 4)
+    # ax_1.plot(l_debug_list)
+    # ax_2.plot(c_debug_list)
+    # ax_3.plot(A_debug_list)
+    # ax_4.plot(nll_debug_list)
+    # plt.show()
     # ========== Debug code ==========
 
     l, c, A = l, c, A
     K = A * kernel_y(y, y, l, d_log_py, d_log_py) + c + A * jnp.eye(n)
-    K_inv = jnp.linalg.inv(K + eps * jnp.eye(n))
+    K_inv = jnp.linalg.inv(K)
     BMC_mean = c * (K_inv @ gy).sum()
     BMC_std = jnp.sqrt(c - K_inv.sum() * c * c)
     pause = True
@@ -176,12 +177,12 @@ def SIR(args, rng_key):
 
     population = float(1e5)
     beta_real, gamma_real = 0.25, 0.05
-    beta_0_test = jnp.linspace(0.01, 0.75, N_test)
+    beta_0_test = jnp.linspace(0.1, 0.8, N_test)
     rate = 10.0
     scale = 1. / rate
     T = 150
     dt = 1.0
-    SamplesNum = 5000
+    SamplesNum = 10000
 
     generate_data_fn = partial(SIR_utils.generate_data, gamma=gamma_real, population=population,
                                T=T, dt=dt, rng_key=rng_key)
@@ -205,7 +206,7 @@ def SIR(args, rng_key):
         ground_truth_array = ground_truth_array.at[i].set(f(temp).mean())
 
     for Nx in Nx_array:
-        beta_0_array = jnp.linspace(0.01, 0.75, Nx)
+        beta_0_array = jnp.linspace(0.1, 0.75, Nx)
 
         for Ny in Ny_array:
             BMC_mean_array = jnp.zeros([Nx])
@@ -342,6 +343,7 @@ def get_config():
 if __name__ == '__main__':
     args = get_config()
     args = create_dir(args)
+    print(f'seed is {args.seed}')
     main(args)
     save_path = args.save_path
     print(f"\nChanging save path from\n\n{save_path}\n\nto\n\n{save_path}__complete\n")
