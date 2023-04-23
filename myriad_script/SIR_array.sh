64db0377ee3328e78875e05f53581416351a4ff3
Zonghao Chen <47094594+hudsonchen@users.noreply.github.com>
2023-04-23 14:44:48 +0100

    myriad

--- myriad_script/SIR_array.sh
+++ myriad_script/SIR_array.sh
@@ -4,4 +4,4 @@ jobs_in_parallel=$(wc -l < "$1")
 echo $jobs_in_parallel
 echo $1
 
-qsub -t 1-${jobs_in_parallel} /home/zongchen/CBQ/scripts/sensitivity_base.sh "$1"
+qsub -t 1-${jobs_in_parallel} /home/ucabzc9/Scratch/CBQ/scripts/SIR_base.sh "$1"
