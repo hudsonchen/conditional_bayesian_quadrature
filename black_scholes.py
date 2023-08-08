7db55bd92d9b75007cc0e0238213fcb4052be90c
hudsonchen <czh17@mails.tsinghua.edu.cn>
2023-08-08 10:53:10 +0100

    minor

--- black_scholes.py
+++ black_scholes.py
@@ -480,8 +480,8 @@ def option_pricing(args):
     rng_key = jax.random.PRNGKey(seed)
     rng_key, _ = jax.random.split(rng_key)
 
-    T_array = jnp.array([10, 20, 30])
-    # T_array = jnp.array([30])
+    # T_array = jnp.array([10, 20, 30])
+    T_array = jnp.array([30])
     # N_array = jnp.array([50, 100])
     N_array = jnp.concatenate((jnp.array([5]), jnp.arange(5, 105, 5)))
 
